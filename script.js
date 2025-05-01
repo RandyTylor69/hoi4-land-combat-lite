1c9311f761453aa6d599755229ac6f0ed193d1ea
Depayss <Christophermaomao@gmail.com>
2025-05-01 16:16:05 -0400

    added alert function "esports click"

---
+++ script.js
@@ -0,0 +1,29 @@
+const panzerDiv = document.querySelector(".tankIcon")
+const infDiv = document.querySelector(".infIcon")
+let isSelected = false
+
+panzerDiv.addEventListener("click", (e) => {
+    e.stopPropagation()
+    selected(document.querySelector(".axisDivisions"))
+})
+
+document.body.addEventListener("click", ()=>{
+    unselect(document.querySelector(".axisDivisions"))
+})
+
+infDiv.addEventListener("contextmenu", ()=>{
+    if (isSelected) {
+        alert("esports click!")
+    }
+})
+
+function selected(div) {
+    div.style.boxShadow = "2px 2px 5px yellow"
+    isSelected = true
+}
+
+function unselect(div) {
+    div.style.boxShadow = "2px 2px 5px black"
+    isSelected = false
+}
+
